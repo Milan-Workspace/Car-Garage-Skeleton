b45ac8dd7d60e7d202f6726c68738040034b1a11
Muhammad Hamza <p2914681@my365.dmu.ac.uk>
2026-05-05 13:27:10 +0100

    new changes by hamza

---
+++ SQLQuery2.sql
@@ -0,0 +1,17 @@
+﻿-- Rename EmployeeID to PartID
+EXEC sp_rename 'dbo.ServiceRecord.EmployeeID', 'PartID', 'COLUMN';
+GO
+
+-- Add foreign key for Vehicle
+ALTER TABLE dbo.ServiceRecord
+ADD CONSTRAINT FK_ServiceRecord_Vehicles
+FOREIGN KEY (VehicleID)
+REFERENCES dbo.Vehicles(VehicleID);
+GO
+
+-- Add foreign key for Part (Inventory)
+ALTER TABLE dbo.ServiceRecord
+ADD CONSTRAINT FK_ServiceRecord_Inventories
+FOREIGN KEY (PartID)
+REFERENCES dbo.Inventories(PartID);
+GO
---
+++ SQLQuery4.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE dbo.ServiceRecord
+ADD CONSTRAINT FK_ServiceRecord_Inventories
+FOREIGN KEY (PartID)
+REFERENCES dbo.Inventories(PartID);
