c47ed0c68263a11948972c5fceeb086f312d0d8d
Polozaix <polozaic@gmail.com>
2026-05-20 02:08:47 +0100

    added customer list form

--- AdminSystem/CustomerList.aspx.cs
+++ AdminSystem/CustomerList.aspx.cs
@@ -4,11 +4,56 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (IsPostBack == false)
+        {
+            Session["CustomerID"] = null;
+            DisplayCustomers("");
+        }
+    }
 
+    void DisplayCustomers(string lastNameFilter)
+    {
+        clsCustomerCollection Customers = new clsCustomerCollection();
+        if (lastNameFilter != "")
+            Customers.ReportByLastName(lastNameFilter);
+        gvCustomers.DataSource = Customers.CustomerList;
+        gvCustomers.DataBind();
+    }
+
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+        DisplayCustomers(txtSearch.Text);
+    }
+
+    protected void btnClear_Click(object sender, EventArgs e)
+    {
+        txtSearch.Text = "";
+        DisplayCustomers("");
+    }
+
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        Session["CustomerID"] = null;
+        Response.Redirect("CustomerDataEntry.aspx");
+    }
+
+    protected void btnEdit_Click(object sender, EventArgs e)
+    {
+        Button btn = (Button)sender;
+        Session["CustomerID"] = btn.CommandArgument;
+        Response.Redirect("CustomerDataEntry.aspx");
+    }
+
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        Button btn = (Button)sender;
+        Session["CustomerID"] = btn.CommandArgument;
+        Response.Redirect("CustomerConfirmDelete.aspx");
     }
 }
