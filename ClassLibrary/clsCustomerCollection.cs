ffab636115dfc21e679b0e8bbfe567d4d085fe22
Polozaix <polozaic@gmail.com>
2026-05-21 11:15:13 +0100

    in process

--- ClassLibrary/clsCustomerCollection.cs
+++ ClassLibrary/clsCustomerCollection.cs
@@ -1,95 +1,124 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace ClassLibrary
 {
-        public class clsCustomerCollection
+    public class clsCustomerCollection
+    {
+        List<clsCustomer> mCustomerList = new List<clsCustomer>();
+        clsCustomer mThisCustomer = new clsCustomer();
+
+        public List<clsCustomer> CustomerList
         {
-            List<clsCustomer> mCustomerList = new List<clsCustomer>();
-            clsCustomer mThisCustomer = new clsCustomer();
+            get { return mCustomerList; }
+            set { mCustomerList = value; }
+        }
 
-            public List<clsCustomer> CustomerList
+        public int Count
+        {
+            get { return mCustomerList.Count; }
+        }
+
+        public clsCustomer ThisCustomer
+        {
+            get { return mThisCustomer; }
+            set { mThisCustomer = value; }
+        }
+
+        public clsCustomerCollection()
+        {
+            PopulateFromDB("");
+        }
+
+        void PopulateFromDB(string lastNameFilter)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                get { return mCustomerList; }
-                set { mCustomerList = value; }
-            }
-
-            public int Count
-            {
-                get { return mCustomerList.Count; }
-            }
-
-            public clsCustomer ThisCustomer
-            {
-                get { return mThisCustomer; }
-                set { mThisCustomer = value; }
-            }
-
-            public clsCustomerCollection()
-            {
-                PopulateFromDB("");
-            }
-
-            void PopulateFromDB(string lastNameFilter)
-            {
-                clsDataConnection DB = new clsDataConnection();
-                 int count = 0;
-
+                SqlCommand cmd;
                 if (string.IsNullOrEmpty(lastNameFilter))
-                    count = DB.Execute("sproc_tbl_CustomerSelectAll");
+                {
+                    cmd = new SqlCommand("sproc_tbl_CustomerSelectAll", conn);
+                }
                 else
                 {
-                    DB.AddParameter("@LastName", lastNameFilter);
-                    count = DB.Execute("sproc_tbl_CustomerFilterByLastName");
+                    cmd = new SqlCommand("sproc_tbl_CustomerFilterByLastName", conn);
+                    cmd.Parameters.AddWithValue("@LastName", lastNameFilter);
                 }
-
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
                 mCustomerList.Clear();
-                foreach (DataRow row in DB.DataTable.Rows)
+                foreach (DataRow row in dt.Rows)
                 {
                     clsCustomer c = new clsCustomer();
                     c.CustomerID = Convert.ToInt32(row["CustomerID"]);
                     c.FirstName = Convert.ToString(row["FirstName"]);
                     c.LastName = Convert.ToString(row["LastName"]);
                     c.Email = Convert.ToString(row["Email"]);
-                    c.Phone = Convert.ToString(row["Phone"]);
+                    c.Phone = Convert.ToString(row["PhoneNumber"]);
                     c.DateRegistered = Convert.ToDateTime(row["DateRegistered"]);
-                    c.IsActive = Convert.ToBoolean(row["IsActive"]);
+                    c.IsActive = Convert.ToBoolean(row["isActive"]);
                     mCustomerList.Add(c);
                 }
             }
-        public int Add()
+        }
+
+        public void Add()
         {
-            clsDataConnection DB = new clsDataConnection();
-            int count = 0;
-            DB.AddParameter("@FirstName", mThisCustomer.FirstName);
-            DB.AddParameter("@LastName", mThisCustomer.LastName);
-            DB.AddParameter("@Email", mThisCustomer.Email);
-            DB.AddParameter("@Phone", mThisCustomer.Phone);
-            count = DB.Execute("sproc_tbl_CustomerAdd");
-            return Convert.ToInt32(DB.DataTable.Rows[0]["CustomerID"]);
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("sproc_tbl_CustomerAdd", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@FirstName", mThisCustomer.FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", mThisCustomer.LastName);
+                    cmd.Parameters.AddWithValue("@Email", mThisCustomer.Email);
+                    cmd.Parameters.AddWithValue("@Phone", mThisCustomer.Phone);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
         public void Update()
         {
-            clsDataConnection DB = new clsDataConnection();
-            DB.AddParameter("@CustomerID", mThisCustomer.CustomerID);
-            DB.AddParameter("@FirstName", mThisCustomer.FirstName);
-            DB.AddParameter("@LastName", mThisCustomer.LastName);
-            DB.AddParameter("@Email", mThisCustomer.Email);
-            DB.AddParameter("@Phone", mThisCustomer.Phone);
-            DB.AddParameter("@IsActive", mThisCustomer.IsActive);
-            DB.Execute("sproc_tbl_CustomerUpdate");
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("sproc_tbl_CustomerUpdate", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@CustomerID", mThisCustomer.CustomerID);
+                    cmd.Parameters.AddWithValue("@FirstName", mThisCustomer.FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", mThisCustomer.LastName);
+                    cmd.Parameters.AddWithValue("@Email", mThisCustomer.Email);
+                    cmd.Parameters.AddWithValue("@Phone", mThisCustomer.Phone);
+                    cmd.Parameters.AddWithValue("@IsActive", mThisCustomer.IsActive);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
         public void Delete()
         {
-            clsDataConnection DB = new clsDataConnection();
-            DB.AddParameter("@CustomerID", mThisCustomer.CustomerID);
-            DB.Execute("sproc_tbl_CustomerDelete");
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("sproc_tbl_CustomerDelete", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@CustomerID", mThisCustomer.CustomerID);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
         public void ReportByLastName(string LastName)
